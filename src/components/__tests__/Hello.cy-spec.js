3e772c0a656548bb19e77d8386edd26ce6e1fdfc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-07-29 16:31:20 -0400

    tweaking tests

--- src/components/__tests__/Hello.cy-spec.js
+++ src/components/__tests__/Hello.cy-spec.js
@@ -3,6 +3,16 @@ import { mount } from 'cypress-react-unit-test';
 
 it('hello world', () => {
   mount(<p>Hello Jest!</p>);
+  // cy.contains('Hello Jest!');
+  // or use @testing-library/cypress command
+  // cy.log('**@testing-library/cypress**');
+  cy.findByText('Hello Jest!');
+});
+
+it('hello world component', () => {
+  // the Command Log will show "<HelloWorld>" instead of "<Unknown>"
+  const HelloWorld = () => <p>Hello Jest!</p>;
+  mount(<HelloWorld />);
   cy.contains('Hello Jest!');
   // or use @testing-library/cypress command
   cy.log('**@testing-library/cypress**');
